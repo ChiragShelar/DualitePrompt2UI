1f829f08e9d8013677847a7e3c846d825b332015
Chirag Shelar <78549656+ChiragShelar@users.noreply.github.com>
2024-08-22 20:23:45 +0530

    Minor update in midjourney.py

--- Midjourney_api/midjourney.py
+++ Midjourney_api/midjourney.py
@@ -1,6 +1,5 @@
 import subprocess
 
-#For more details follow this github link: https://github.com/George-iam/Midjourney_api
 
 def generate_image(prompt):
     command = f'python Midjourney_api\\sender.py --params Midjourney_api\\sender_params.json --prompt "{prompt}"'
